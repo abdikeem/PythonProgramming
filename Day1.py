2c4218f2e91602a59a1d9e55c2b0b092db20a44d
abdikeem <abdihakimabdirahman21@gmail.com>
2022-01-09 18:12:22 +0300

    intial commit

---
+++ Day1.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Welcome to KimzaTech company")
+name = input("whats your name? \n")
+print(f"Hello, {name}")
+#2. Ask the user for the city that they grew up in.
+city_you_grewup = input("Where did you grow up? \n")
+#3. Ask the user for the name of a pet.
+pet_name = input("What is the name of your pet? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("You grew-up at " + city_you_grewup + " and your pet's name is " + pet_name)
+
+#5. Make sure the input cursor shows on a new line, see the example at:
