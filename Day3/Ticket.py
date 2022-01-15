56ae804f845c47a9a2d3d582e1eb084629e29992
abdikeem <abdihakimabdirahman21@gmail.com>
2022-01-15 13:37:50 +0300

    If_statement

--- Day3/Ticket.py
+++ Day3/Ticket.py
@@ -1,12 +1,30 @@
+from functools import total_ordering
+
+
 print("Welcome to the rollercoaster! ")
 height = int(input("What is your hieght in cm? "))
+adult_ticket = 12
+children_ticket = 5
+youth_ticket = 7
+photography = 3
+bill = 0
 if height >= 120:
     print("You can ride a rollercoaster!")
 
     age = int(input("What is you Age? "))
-    if age >= 18:
-        print("Your ticket payment is $12 ")
+    if age <= 12:
+        bill = 5
+        print(f"Child ticket payment is ${children_ticket} ")
+    elif age <= 23:
+        bill = 7
+        print(f"Youth ticket payment is ${youth_ticket}")
     else:
-        print("Your ticket payment is $7 ")
+        bill = 12
+        print(f"Adult ticket payment is ${adult_ticket} ")
+    photo = input("You want to take images? Y Or N? ")
+    if photo == "y":
+        bill += 3
+        print(f"You will be charged ${photography} extra! Enjoy your day")
+    print(f"your total bill is ${bill}")
 else:
     print("Ops! You can't ride on a rollercoaster") 
