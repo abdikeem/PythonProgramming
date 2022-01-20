74950f1e0f5ab02cc91c9b0c3b64bafafff357f6
abdikeem <abdihakimabdirahman21@mail.com>
2022-01-20 10:20:37 +0300

    final project

--- Day3/main.py
+++ Day3/main.py
@@ -24,3 +24,23 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
+
+choice_1 = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right": ').lower()
+if choice_1 == "left":
+    choice_2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.: ').lower()
+    if choice_2 == "wait":
+        choice_3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?: ").lower()
+        if choice_3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice_3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice_3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")   
+else:
+    print("You fell into a hole. Game Over.")
+
+
