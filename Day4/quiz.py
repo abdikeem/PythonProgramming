bb43e1ab1fb8bf7edf7ea47eb39846629d271631
abdikeem <abdihakimabdirahman21@mail.com>
2022-01-24 18:16:53 +0300

    Randomization

--- Day4/quiz.py
+++ Day4/quiz.py
@@ -1,3 +1,10 @@
+# Random module in Python, which is used to generate pseudo-random numbers for various probabilistic distructions
+
+# 1. seed() -> This initializes a random number generator.
+# 2. getstate() -> This returns an object containing the current state of the generator.
+# 3. setstate(state_obj) -> This restores the state of the generator at the point when "getstate()" was called, by passing the state object.
+# 4. getrandbits(k) -> Return a python integer with "k" random bits.
+
 import random
  
 number_genrator = random.uniform(0, 1)
